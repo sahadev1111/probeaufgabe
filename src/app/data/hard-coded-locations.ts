ae2db882f252ce95d43cff112813b06b58096fb6
samuelhausler <haeuslersamuel@gmail.com>
2024-10-13 21:32:20 +0200

    add more hardcoded data

--- src/app/data/hard-coded-locations.ts
+++ src/app/data/hard-coded-locations.ts
@@ -1,6 +1,6 @@
 import {MapLocation, MapItemType} from "../model/map-location.model";
 
-export const hardCodedLocations = [
+export const hardCodedLocations: MapLocation[] = [
   // new POI(9.27, 47.385971, 'Kebabhaus')
   {
     id: 1,
@@ -33,6 +33,71 @@ export const hardCodedLocations = [
     attributes: {
       category: "parking",
       description: "expensive, avoid"
+    }
+  },
+  {
+    id: 4,
+    name: 'Bäckerei Knöpfel',
+    type: MapItemType.POI,
+    longitude: 9.278666,
+    latitude: 47.385920,
+    attributes: {
+      category: "Bäckerei",
+      description: "good bread"
+    }
+  },
+  {
+    id: 5,
+    name: 'Strassenlaterne',
+    type: MapItemType.StreetLight,
+    longitude: 9.279266,
+    latitude: 47.385449,
+    attributes: {
+      category: "parking",
+      description: "expensive, avoid"
+    }
+  },
+  {
+    id: 6,
+    name: 'Strassenlaterne',
+    type: MapItemType.StreetLight,
+    longitude: 9.279214,
+    latitude:47.385517,
+    attributes: {
+      category: "parking",
+      description: "expensive, avoid"
+    }
+  },
+  {
+    id: 7,
+    name: 'Strassenlaterne',
+    type: MapItemType.StreetLight,
+    longitude: 9.279167,
+    latitude:47.385590,
+    attributes: {
+      category: "parking",
+      description: "expensive, avoid"
+    }
+  },
+  {
+    id: 8,
+    name: 'Strassenlaterne',
+    type: MapItemType.StreetLight,
+    longitude: 9.385712,
+    latitude:47.278945,
+    attributes: {
+      category: "parking",
+      description: "expensive, avoid"
+    }
+  },
+  {
+    id: 9,
+    name: 'Geburtsort',
+    type: MapItemType.LifeStage,
+    longitude: 9.283373,
+    latitude:47.387932,
+    attributes: {
+      description: "Here I was born"
     }
   }
 ];
